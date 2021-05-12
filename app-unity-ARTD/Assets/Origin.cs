2556c25f5566fcf0f600e32af73cfe0d767ad1d1
Sean Lavery <theseanlavery@gmail.com>
2021-05-11 23:09:21 -0700

    add spawn functionality to origin

---
+++ app-unity-ARTD/Assets/Origin.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Origin : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (Instance != null)
+        {
+            Destroy(Instance.gameObject);
+        }
+        Instance = this;
+    }
+
+    public static Origin Instance;
+
+    private void Update()
+    {
+        if (GameManager.Instance.gameReady)
+        {
+            
+            
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Enemy enemy =  Instantiate(EnemyPrefabs[0],transform.position,transform.rotation);
+        
+        
+
+    }
+
+    public List<Enemy> EnemyPrefabs = new List<Enemy>();
+    private IEnumerator WavePattern01()
+    {
+        while (GameManager.Instance.EnemiesLeft > 0)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1);    
+        }
+        
+        yield return null;
+    }
+    private IEnumerator WavePattern02()
+    {
+        
+        yield return null;
+    }
+}
