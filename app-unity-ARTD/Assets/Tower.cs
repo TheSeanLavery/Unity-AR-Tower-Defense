2a708e40036795ba96df992d0e56f3efe496ed01
Sean Lavery <theseanlavery@gmail.com>
2021-05-11 22:48:14 -0700

    add tower stub script

---
+++ app-unity-ARTD/Assets/Tower.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    public enum damageType
+    {
+        piercing,aoe
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
