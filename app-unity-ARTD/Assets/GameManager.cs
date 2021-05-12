c4cc6a8fb3b7b0c1b4d437ea3e2be14d3b4ee709
Sean Lavery <theseanlavery@gmail.com>
2021-05-11 23:09:39 -0700

    add ready state to gamemanager

--- app-unity-ARTD/Assets/GameManager.cs
+++ app-unity-ARTD/Assets/GameManager.cs
@@ -5,14 +5,47 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        Instance = this;
     }
 
+    public static GameManager Instance;
+
+
+    public GameObject origin;
+    public GameObject destination;
+
+    public int CurrentWave;
+
+    public int EnemiesLeft;
+    
+    
+    
+    
+    
+    
+    public void ResetGame()
+    {
+        Destroy(origin);
+        Destroy(destination);
+        CurrentWave = 0;
+        EnemiesLeft = 0;
+    }
+
+
+    public bool gameReady = false;
     // Update is called once per frame
     void Update()
     {
-        
+        if (origin != null && destination != null)
+        {
+            gameReady = true;
+
+        }
+        else
+        {
+            gameReady = false;
+        }
     }
 }
