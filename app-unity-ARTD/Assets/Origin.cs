aae2c3c00c10bb1f9172d4db2e05ba6d641a6f72
Sean Lavery <theseanlavery@gmail.com>
2021-05-12 22:22:33 -0700

    setup wave spawning

--- app-unity-ARTD/Assets/Origin.cs
+++ app-unity-ARTD/Assets/Origin.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class Origin : MonoBehaviour
 {
@@ -15,8 +16,23 @@ public class Origin : MonoBehaviour
         Instance = this;
 
         GameManager.Instance.origin = Instance;
+        
+        
+        waves.Clear();
+        waves.Add(WavePattern01);
+        waves.Add(WavePattern02);
+    }
+
+    private List<Func<IEnumerator>> waves = new List<Func<IEnumerator>>();
 
 
+    public void StartWave(int wave)
+    {
+        Func<IEnumerator> wavePattern = waves[wave];
+        if (wavePattern != null)
+        {
+            StartCoroutine(wavePattern());
+        }
     }
 
     public static Origin Instance;
@@ -32,15 +48,30 @@ public class Origin : MonoBehaviour
 
     private void SpawnEnemy()
     {
-        Enemy enemy =  Instantiate(EnemyPrefabs[0],transform.position,transform.rotation);
-        
+        int randomEnemy = (int)(EnemyPrefabs.Count * Random.Range(0f, 1f));
+        Enemy enemy =  Instantiate(EnemyPrefabs[randomEnemy],transform.position,transform.rotation);
+        spawnedEnemies.Add(enemy);
         
 
     }
 
+    public void DestroyEnemies()
+    {
+        for (int i = spawnedEnemies.Count-1; i >= 0; i--)
+        {
+            Enemy e = spawnedEnemies[i];
+            spawnedEnemies.Remove(e);
+            Destroy(e.gameObject);
+        }
+    }
+
+    public List<Enemy> spawnedEnemies = new List<Enemy>();
+
     public List<Enemy> EnemyPrefabs = new List<Enemy>();
+    
     private IEnumerator WavePattern01()
     {
+        GameManager.Instance.EnemiesLeft = 20;
         while (GameManager.Instance.EnemiesLeft > 0)
         {
             SpawnEnemy();
@@ -51,7 +82,13 @@ public class Origin : MonoBehaviour
     }
     private IEnumerator WavePattern02()
     {
+        GameManager.Instance.EnemiesLeft = 20;
         
+        while (GameManager.Instance.EnemiesLeft > 0)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(1);    
+        }
         yield return null;
     }
 }
