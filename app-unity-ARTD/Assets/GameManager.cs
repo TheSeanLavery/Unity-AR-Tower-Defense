df18605933386f2c35a79d4d1a3d69f59c8b4704
Sean Lavery <theseanlavery@gmail.com>
2021-05-13 02:06:24 -0700

    setup UI and gamestates

--- app-unity-ARTD/Assets/GameManager.cs
+++ app-unity-ARTD/Assets/GameManager.cs
@@ -36,19 +36,43 @@ public class GameManager : MonoBehaviour
         InterWave
     }
 
+    public GameState gameState = GameState.Scanning;
+
     public void StartGame()
     {
+        StartCoroutine(GameStartCoroutine());
 
+    }
+
+    public IEnumerator GameStartCoroutine()
+    {
+        SoftReset();
         if (CheckGameReady())
         {
             ResetVariables();
-            StartWave(CurrentWave);
+            gameState = GameState.Countdown;
+            TimeLeftText.text = "Wave Starts in: " +3;
+            yield return new WaitForSeconds(1);
+            TimeLeftText.text = "Wave Starts in: " +2;
+            yield return new WaitForSeconds(1);
+            TimeLeftText.text = "Wave Starts  in: " +1;
+            yield return new WaitForSeconds(1);
+            TimeLeftText.text = "START!";
+            StartWave(CurrentWave); // gamestate is set inside Startwave
+            yield return new WaitForSeconds(3);
+            TimeLeftText.text = "";
+
+            while (EnemiesLeft > 0) yield return null;
+
+            TimeLeftText.text = "Wave Complete";
         }
         else
         {
 
             GameNotReady();
         }
+
+        yield return null;
     }
 
     public void ResetVariables()
@@ -56,7 +80,7 @@ public class GameManager : MonoBehaviour
         CurrentWave = 0;
         EnemiesLeft = 0;
         currentScore = 0;
-        currentCoins = 0;
+        currentCoins = 20;
         Health = 5;
         
     }
@@ -71,19 +95,59 @@ public class GameManager : MonoBehaviour
 
     public void ResetGame()
     {
-        origin.DestroyEnemies();
+        SoftReset();
         Destroy(origin.gameObject);
         Destroy(destination.gameObject);
-        ResetVariables();
+        
     }
-    private int currentScore;
+
+    public void SoftReset()
+    {
+        origin.DestroyEnemies();
+        DestroyAllTowers();
+        ResetVariables();
+        DestroyAllProjectiles();
+    }
+
+    public void DestroyAllTowers()
+    {
+        for (int i= SpawnedTowers.Count - 1; i >= 0; i--)
+        {
+            Tower t = SpawnedTowers[i];
+
+            SpawnedTowers.RemoveAt(i);
+            Destroy(t.gameObject);
+
+        }
+        
+    }
+
+
+    public List<GameObject> Projectiles = new List<GameObject>();
+
+    public void DestroyAllProjectiles()
+    {
+        for (int i = Projectiles.Count - 1; i >= 0; i--)
+        {
+            var p = Projectiles[i];
+            
+            Projectiles.RemoveAt(i);
+            Destroy(p);
+
+        }
+        
+    }
+    
+    public int currentScore;
     public float defaultSpeedOfEnemies = 1;
 
     [SerializeField] public List<EnemyList> WaveList = new List<EnemyList>();
     public void StartWave(int wave)
     {
+        gameState = GameState.Playing;
         origin.EnemyPrefabs = WaveList[wave].Enemies;
         origin.StartWave(wave);
+        
     }
     
     public bool gameReady = false;
@@ -135,6 +199,12 @@ public class GameManager : MonoBehaviour
         CoinsText.text = "Coins: " + currentCoins;
         RemainingText.text = "Remaining: " + EnemiesLeft;
         scoreText.text = "Score: " + currentScore;
+
+        if (gameState == GameState.Playing)
+        {
+            
+        }
+        
     }
 
     public  TMP_Text HealthText;
@@ -142,9 +212,32 @@ public class GameManager : MonoBehaviour
     public  TMP_Text WaveText;
     public  TMP_Text CoinsText;
     public  TMP_Text RemainingText;
+    public TMP_Text TimeLeftText;
 
     public GameObject GameOverPanel;
+    public List<Tower> SpawnedTowers;
 
+    public Rigidbody getClosestEnemy(Vector3 transformPosition)
+    {
+        float distance = float.MaxValue;
+        Enemy closestEnemy = null;
+        foreach (var e in origin.spawnedEnemies)
+        {
+            var d = Vector3.Distance(e.transform.position, transformPosition);
+            if (d < distance)
+            {
+
+                distance = d;
+                closestEnemy = e;
+            }
+        }
+
+        if (closestEnemy == null)
+        {
+            return null;
+        }
+        return closestEnemy.rb;
+    }
 }
 
 [Serializable]
