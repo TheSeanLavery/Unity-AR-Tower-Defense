4524134bbe87a0a31228256f45281b85f1be24a8
Sean Lavery <theseanlavery@gmail.com>
2021-05-12 20:39:21 -0700

    set enemy to move towards destination

--- app-unity-ARTD/Assets/Enemy.cs
+++ app-unity-ARTD/Assets/Enemy.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -9,18 +10,40 @@ public class Enemy : MonoBehaviour
     {
         speed = DefaultSpeed;
         Health = MaxHealth;
+        
+        rb = GetComponent<Rigidbody>();
+        
+        
+        //TODO turn this into a method inside GameManager
+        GameManager.Instance.AliveEnemies.Add(this);
+        
     }
 
-    public int Health = 100;
-    public int MaxHealth = 100;
+    private Rigidbody rb;
+    
+    
+
+    public float Health = 1;
+    public float MaxHealth = 1;
     public float speed = 1;
     public float DefaultSpeed = 1;
 
+    
+    public void Kill()
+    {
+        //TODO turn this into a method inside GameManager
+        GameManager.Instance.AliveEnemies.Remove(this);
+    }
 
     public Vector3 targetLocation;
     // Update is called once per frame
     void Update()
     {
+        var T = transform;
+        T.LookAt(GameManager.Instance.destination.transform);
         
+        rb.velocity = T.forward * speed;
+
+
     }
 }
